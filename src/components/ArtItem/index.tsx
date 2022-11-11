d308f738b5c09b111526e3aec9b942d90186aaad
mohiuddinmh <mohiuddinmh@gmail.com>
2022-11-11 09:37:17 -0600

    show img only when artwork is present

--- src/components/ArtItem/index.tsx
+++ src/components/ArtItem/index.tsx
@@ -2,27 +2,17 @@ import React, { useEffect, useState } from "react"
 import { getArtwork } from "../../api";
 import { getImageUrl } from "../../utils";
 import Rater from "../Rater";
+import { Artwork } from "../../types";
 
 interface ArtProps {
   id: number
   disabled: boolean
 }
 
-interface Artwork {
-  data: {
-    title: string
-    artist_title: string
-    image_id: string
-  }
-}
-
 const ArtItem = ({ id, disabled }: ArtProps) => {
   const [voted, setVoted] = useState<boolean>(false)
   const [artwork, setArtwork] = useState<Artwork | null>(null)
-
-
   const [rating, setRating] = useState<number>()
-
 
   useEffect(() => {
     getArtwork(id)
@@ -30,22 +20,21 @@ const ArtItem = ({ id, disabled }: ArtProps) => {
       .then(json => setArtwork(json))
   }, [id])
 
-
   if (disabled) {
     return <></>
   }
 
-
   return (
     <div className="item">
-      <h2>{artwork?.data.title}</h2>
-
-      <h3>{artwork?.data.artist_title}</h3>
-
-      <img alt='art image' style={{ width: 100 }} src={getImageUrl(artwork?.data.image_id)}/>
-
       {artwork && <>
+        <h2>{artwork.data.title}</h2>
+
+        <h3>{artwork.data.artist_title}</h3>
+
+        <img alt='art image' style={{ width: 100 }} src={getImageUrl(artwork.data?.image_id)}/>
+
         <p>Rating: {rating}</p>
+
         {!voted && <Rater {...{ setRating, setVoted }} />}
       </>}
     </div>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,12 @@
-export interface  Art {
+export interface Art {
   id: number
   disabled: boolean
+}
+
+export interface Artwork {
+  data: {
+    title: string
+    artist_title: string
+    image_id: string
+  }
 }
